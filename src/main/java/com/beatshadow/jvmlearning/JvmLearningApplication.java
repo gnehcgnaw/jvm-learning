8e75bc64ebb6a1bac5fc6b0607614fd3f37555e2
gnehcgnaw <gnehcgnaw@gamil.com>
2020-04-09 14:32:09 +0800

    init

---
+++ src/main/java/com/beatshadow/jvmlearning/JvmLearningApplication.java
@@ -0,0 +1,13 @@
+package com.beatshadow.jvmlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beatshadow/jvmlearning/JvmLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beatshadow.jvmlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
